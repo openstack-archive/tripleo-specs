07990ebc8e77308bb3ee886b1e2c09df7152c3e2
Doug Hellmann <doug@doughellmann.com>
2014-09-10 15:16:07 -0400

    Add RSS feed
    Publish an RSS feed of the changes to the specs repository to make the
    specs more discoverable.
    
    Change-Id: Ib4f84efab8b3af3ddd47d38d63a91362612bd655

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,8 +23,13 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = [
     'sphinx.ext.autodoc',
     #'sphinx.ext.intersphinx',
-    'oslosphinx'
+    'oslosphinx',
+    'yasfb',
 ]
+
+# Feed configuration for yasfb
+feed_base_url = 'http://specs.openstack.org/openstack/tripleo-specs'
+feed_author = 'OpenStack TripleO Team'
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
 # text edit cycles.
